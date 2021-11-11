6dbae770e2a150a55f552977f8ca563cba49d499
xvferdy <xv.ferdy@gmail.com>
2021-11-11 09:17:30 +0800

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import "./stylesheets/css/style.css";
 
 function App() {
 	return (
---
+++ src/pages/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+	return <div>-profil diri</div>;
+}
+
+export default About;
